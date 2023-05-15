d66f2fc5bc9ff020093951b7e5ca017b18fc38b9
marisa2222 <msharifi2222@gmail.com>
2023-05-15 09:29:58 +0200

    added .env.js and package.json.js to server

---
+++ server/.env.js
@@ -0,0 +1,19 @@
+# Port number
+PORT=4002
+
+# Origin URL
+# i.e.: https://www.example.com
+# i.e.: http://localhost:3000
+ORIGIN_URL=YOUR_FRONTEND_URL
+
+# MongoDB Atlas URI
+DB_Name="TEST-DB"
+ATLAS_URI="mongodb+srv://marisa2222:BDitZqVOSTyP4b2q@cluster0.76rhtjj.mongodb.net/?retryWrites=true&w=majority"
+
+# JWT secret
+TOKEN_SECRET="JWT_TOKEN_SECRET"
+
+# Cludinary credentials
+CLOUD_NAME="dtrwpivss"
+API_KEY="162661121943618"
+API_SECRET="3lY0SarQYGKHLvaq92dIQk6Vwe8"
